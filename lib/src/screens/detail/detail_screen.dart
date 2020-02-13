4c6c7a099f163f6364f33b15f521046afb32fb1f
antonio02 <antonio.filho.01@hotmail.com>
2020-02-13 16:30:21 -0300

    text scroll

--- lib/src/screens/detail/detail_screen.dart
+++ lib/src/screens/detail/detail_screen.dart
@@ -15,18 +15,63 @@ class _DetailScreenState extends State<DetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Hero(
-        tag: 'image${widget.anime.id}',
-        child: AspectRatio(
-          aspectRatio: 16 / 10,
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: NetworkImage(widget.anime.coverUrl),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 200,
+              automaticallyImplyLeading: false,
+              floating: false,
+              pinned: true,
+              flexibleSpace: Stack(
+                children: <Widget>[
+                  Hero(
+                    tag: 'image${widget.anime.id}',
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(widget.anime.coverUrl),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomLeft,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: <Color>[Colors.transparent, Colors.black],
+                      ),
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        IconButton(
+                          onPressed: () => Navigator.pop(context),
+                          icon: Icon(Icons.arrow_back, color: Colors.white,),
+                        ),
+                        Expanded(
+                          child: SingleChildScrollView(
+                            padding: EdgeInsets.only(right: 8),
+                            scrollDirection: Axis.horizontal,
+                            child: Text(
+                              widget.anime.title,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(color: Colors.white, fontSize: 22),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
-          ),
+          ];
+        },
+        body: Center(
+          child: Text("Sample Text"),
         ),
       ),
     );
